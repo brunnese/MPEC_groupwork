33a615ccabb8461f8ff0ab02dd2001c4334643aa
dograf <42509345+dograf@users.noreply.github.com>
2021-05-21 11:26:24 +0200

    Add files via upload

---
+++ GroupWork1/Dominik/NMPC_Matlab_singlestep.m
@@ -0,0 +1,114 @@
+function p = NMPC_Matlab_singlestep(qp_W,qp_gradJ,qp_gradhT,qp_h,FJk,...
+                                 parNMPC,parSim)
+
+%% Options
+Ts        	= parNMPC.Ts;            % Sampling time [s]
+tFinal     	= parSim.Tsim;           % Final time [s]
+x0       	= parNMPC.x0;            % Initial condition of system
+nRK4        = parNMPC.nRK4;          % Number of RK4 intervals per time step
+
+% Parse options
+N           = parNMPC.N;             % Horizon length
+time = (0:Ts:tFinal)';               % Time vector (for plotting)
+
+%% Solve the NLP using SQP
+% Create CasADi functions for QPs
+[qp_W,qp_gradJ,qp_gradhT,qp_h,lb,ub] = createCasadiFunctions(parNMPC);
+
+% Initialization
+nStates     = parNMPC.nStates;
+nInputs     = parNMPC.nInputs;
+nOptVars    = (nStates+nInputs)*parNMPC.N;
+nConstr     = (nStates)*parNMPC.N;
+optVars   	= zeros(nOptVars,1);
+p           = zeros(13,1);
+lambda      = zeros(nConstr,1);     % Dual variables for linear inequality
+                                    % constraints (as used in qpOASES):
+                                    % lbA <= Ax <= ubA
+mu          = zeros(nOptVars,1);    % Dual variables for lower and upper 
+                                    % bounds on optimization variables:
+                                    % lb <= x <= ub
+kktCond     = Inf*ones(nOptVars+nConstr,1);
+kSQP     	= 1;
+
+% Options
+kktTol 	= parNMPC.kktTol;      	% Stopping criteria for KKT conditions
+alpha 	= parNMPC.alphaSQP;        	% Step length for damped Newton's method
+maxIter = parNMPC.QPMaxIter;        	% Maximum SQP iterations
+
+while (norm(kktCond,2) >= kktTol) && (kSQP <= maxIter)
+    
+    % Get numerical values for QP matrices/vectors and bounds
+    W       = full(qp_W(optVars,[p;lambda]));
+    gradJ 	= full(qp_gradJ(optVars,p));
+    gradhT  = full(qp_gradhT(optVars,p));
+    h     	= full(qp_h(optVars,p));
+    
+    % Set proper bounds for s_xk = x(k+1) - x(k)
+    lb_sxk 	= lb - optVars;
+    ub_sxk	= ub - optVars;
+    
+    % Evaluation of KKT conditions
+    kktCond = [gradJ - gradhT'*lambda - mu; h];
+    
+    % Solve the QP
+    if kSQP==1
+        [QP,sQP,~,exitflag,~,lambdaQP,~] = qpOASES_sequence('i',W,gradJ,...
+            gradhT,lb_sxk,ub_sxk,-h,-h);
+    else
+        [sQP,~,exitflag,~,lambdaQP,~] = qpOASES_sequence('m',QP,W,gradJ,...
+            gradhT,lb_sxk,ub_sxk,-h,-h);
+    end % if
+    lambda_OptVars  = lambdaQP(1:length(lb_sxk));
+    lambda_AOptVars = lambdaQP(length(lb_sxk)+1:end);
+    
+    % Update optimisation variables and lambda/mu
+    xprev   = optVars;                                            % for debugging
+    sx      = sQP;
+    smu     = lambda_OptVars - mu;
+    slambda = lambda_AOptVars - lambda;
+    optVars = optVars + alpha*sx;
+    mu      = mu + alpha*smu;
+    lambda  = lambda + alpha*slambda;
+    
+    % Figure during loop (for debugging)
+%     if 0
+%         figure(99); clf;
+%         ax(1) = subplot(2,1,1); hold on; box on; grid on;
+%         plot([0 nOptVars],-0.25*[1 1],'b--');
+%         plot([0:3:nOptVars],[NaN; lb(2:3:end)],'b-');
+%         plot([0:3:nOptVars],[options.x0(1); xprev(2:3:end)],'bx');
+%         plot([0:3:nOptVars],[options.x0(1); optVars(2:3:end)],'b.','MarkerSize',10);
+%         plot([0:3:nOptVars],[options.x0(2); xprev(3:3:end)],'rx');
+%         plot([0:3:nOptVars],[options.x0(2); optVars(3:3:end)],'r.','MarkerSize',10);
+%         plot([0:3:nOptVars],[NaN; sQP(3:3:end)],'ko','LineWidth',1);
+%         ylabel('$x$');
+%         ax(2) = subplot(2,1,2); hold on; box on; grid on;
+%         plot([0 nOptVars],-[1 1],'k--');
+%         plot([0 nOptVars],[1 1],'k--');
+%         plot([0:3:nOptVars],[lb(1:3:end); NaN],'k-');
+%         plot([0:3:nOptVars],[ub(1:3:end); NaN],'k-');
+%         plot([0:3:nOptVars],[xprev(1:3:end); NaN],'kx');
+%         plot([0:3:nOptVars],[optVars(1:3:end); NaN],'k.','MarkerSize',10);
+%         ylabel('$u$');
+%         xlabel('Index $k$');
+%         disp(exitflag);
+%         pause(0.5);
+%     end % if
+%     
+%     % Display iteration
+%     fprintf('SQP iteration: %i\n',kSQP);
+    
+    % Update iteration index
+    kSQP = kSQP + 1;
+    
+end % while
+
+% Extract solution
+optVarsOpt = optVars; clear x;
+uOpt = [optVarsOpt(1:3:end); NaN];
+x1Opt = [parNMPC.x0(1); optVarsOpt(2:3:end)];
+x2Opt = [parNMPC.x0(2); optVarsOpt(3:3:end)];
+
+
+end
---
+++ GroupWork1/Dominik/createCasadiFunctions.m
@@ -0,0 +1,155 @@
+function [qp_W,qp_gradJ,qp_gradhT,qp_h,lb,ub] = createCasadiFunctions(parNMPC)
+import casadi.*
+%% Definition of model dynamics and objective function (continuous)
+
+% States and inputs
+nStates  = parNMPC.nStates;
+nInputs  = parNMPC.nInputs;
+nOutputs = parNMPC.nOutputs;
+
+x        = MX.sym('x',nStates,1);
+%u        = MX.sym('u',nInputs,1);
+r        = MX.sym('r',2,1);
+du       = MX.sym('du',nInputs,1);
+Q1       = MX.sym('Q1');
+Q2       = MX.sym('Q2');
+R1       = MX.sym('R1');
+R2       = MX.sym('R2');
+
+% Pre-define parameters
+p = MX.sym('p',nStates+nInputs+nOutputs+4); %p = [x0;uprev;r;Q1;Q2;R1;R2]
+
+[xdot, y] = ROModel(x, du);
+
+% System dynamics
+x1dot   = xdot(1); % n_tc
+x2dot   = xdot(2); % p_im
+x3dot   = xdot(3); % p_em
+x4dot   = xdot(4); % F_im
+x5dot   = xdot(5); % F_em
+xdot    = [x1dot; x2dot; x3dot; x4dot; x5dot];
+
+% Objective function
+J       = Q1*(y(1) - r(1))^2 + Q2*(y(2) - r(2))^2 ...
+    + R1*(du(1))^2 + R2*(du(2))^2;
+
+% Create CasADI functions
+fxdot 	= Function('fxdot', {x,du}, {xdot});
+fJ      = Function('fJ', {x,du,r,[Q1;Q2;R1;R2]}, {J});
+
+%% Integration/discretization using RK4
+xStart  = MX.sym('xStart',nStates,1);   % Initial condition of integration
+du       = MX.sym('du',nInputs,1);        % Control input
+TRK4    = parNMPC.Ts/parNMPC.nRK4;      % Step size of each RK4 interval
+
+% Loop over intervals
+xEnd    = xStart;                       % Initialization
+JEnd    = 0;                            % Initialization
+for l = 1:parNMPC.nRK4
+    k1x = fxdot(xEnd, du);
+    k2x = fxdot(xEnd + TRK4/2 * k1x, du);
+    k3x = fxdot(xEnd + TRK4/2 * k2x, du);
+    k4x = fxdot(xEnd + TRK4 * k3x, du);
+    k1J = fJ(xEnd, du, r, [Q1;Q2;R1;R2]);
+    k2J = fJ(xEnd + TRK4/2 * k1x, du, r, [Q1;Q2;R1;R2]);
+    k3J = fJ(xEnd + TRK4/2 * k2x, du, r, [Q1;Q2;R1;R2]);
+    k4J = fJ(xEnd + TRK4 * k3x, du, r, [Q1;Q2;R1;R2]);
+    xEnd = xEnd + TRK4/6 * (k1x + 2*k2x + 2*k3x + k4x);
+    JEnd = JEnd + TRK4/6 * (k1J + 2*k2J + 2*k3J + k4J);
+end % for
+
+% Create CasADi functions
+fxDisc  = Function('fxDisc', {xStart,du}, {xEnd});
+fJDisc  = Function('fJDisc', {xStart,du, r, [Q1;Q2;R1;R2]}, {JEnd});
+
+%% Construct NLP
+% Initialization
+optVars  	= [];   % Vector of optimization variables (i.e. states and inputs)
+optVars0  	= [];   % Initial guess for optimization variables
+lb          = [];   % Lower bound of optimization variables
+ub          = [];   % Upper bound of optimization variables
+Jk          =  0;   % Initialization of objective function
+h           = [];   % Equality constraints
+
+% Pre-define CasADi variables
+U = MX.sym('U_',nInputs,1,parNMPC.N);
+S = MX.sym('S_',nStates,1,parNMPC.N+1);
+
+% Construct NLP step-by-step
+for k = 1:parNMPC.N+1
+    
+    % System dynamics and objective function
+    if k==parNMPC.N+1
+        % Skip, because at final time step, no further integration of 
+        % system dynamics necessary.
+    else
+        % Integration of system dynamics and objective function
+        if k==1
+            % Hardcoded initial condition
+            XEnd = fxDisc(p(1:5),U{k}-p(6:7));
+            Jk = Jk + fJDisc(p(1:5),U{k}-p(6:7),p(8:9),p(10:13));
+        else
+            XEnd = fxDisc(S{k},U{k}-U{k-1});
+            Jk = Jk + fJDisc(S{k},U{k}-U{k-1},p(8:9),p(10:13));
+        end % if
+        
+        % Add equality constraint for continuity (i.e. closing gaps):
+        h = [h; XEnd-S{k+1}];
+        
+    end % if
+    
+    % States
+    if k==1
+        % Skip, because we have hardcoded the initial condition above
+    else
+        % Add states to vector of optimization variables
+        optVars = [optVars; S{k}];
+        
+        % Lower- and upper bounds for states
+        lb = [lb; -Inf; -Inf; -Inf; -Inf; -Inf];
+        ub = [ub; Inf; Inf; Inf; Inf; Inf];
+        
+        % Add initial guess of states
+        optVars0 = [optVars0; p(1:5)];
+        
+    end % if
+    
+    % Inputs
+    if k==parNMPC.N+1
+        % Skip, because no control input at final time step
+    else
+        % Add inputs to vector of optimization variables
+        optVars = [optVars; U{k}];
+        
+        % Lower- and upper bounds for inputs
+        lb = [lb; 0; 0];
+        ub = [ub; 1; 1];
+        
+        % Add initial guess for inputs
+        optVars0 = [optVars0];
+        
+    end % if
+    
+end % for
+
+%% Preparation for SQP
+
+% Construct Lagrange function
+lambda  = MX.sym('lambda',numel(h));
+L       = Jk + lambda'*h;
+
+% Calculate the hessian and jacobian of the objective function and the 
+% (in-)equality constraints using CasADi
+W       = hessian(L,optVars);
+gradJ   = jacobian(Jk,optVars)';
+gradhT 	= jacobian(h,optVars);
+
+% Create CasADi functions
+qp_W        = Function('qp_W',{optVars,[p;lambda]},{W});
+qp_gradJ  	= Function('qp_gradJ',{optVars,p},{gradJ});
+qp_gradhT	= Function('qp_gradhT',{optVars,p},{gradhT});
+qp_h     	= Function('qp_h',{optVars,p},{h});
+
+end % function
+
+% EOF
